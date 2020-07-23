3be57d2335e03716a919539ed515c0a8b744a09a
Rhett Miller <rmiller@sharpencx.com>
2020-07-22 21:41:57 -0400

    setup and the first four sections for the overview page

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 include_once('_parts/header.php');
 ?>
-<!-- RAW HTML HERE -->
-<section class="hello_module"><h1>Hello</h1></section>
-<!-- Or if you want to be fancy, you can include the html file. -->
-<?php include('world-section.html'); ?>
+
+<link rel="stylesheet" type="text/css" href="./bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="./sections.css" />
+
+<!-- rhett is gettin' fancy -->
+<?php include('4.html'); ?>
 
 <?php
 include_once('_parts/footer.php');
