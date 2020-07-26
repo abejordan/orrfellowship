4311a343342fed47b39cb202ad09fdd03c9f3ea6
Rhett Miller <rmiller@sharpencx.com>
2020-07-26 13:05:42 -0400

    finished tiles responsiveness

--- index.php
+++ index.php
@@ -6,7 +6,11 @@ include_once('_parts/header.php');
 <link rel="stylesheet" type="text/css" href="./sections.css" />
 
 <!-- rhett is gettin' fancy -->
+<?php include('1.html'); ?>
+<?php include('2.html'); ?>
+<?php include('3.html'); ?>
 <?php include('4.html'); ?>
+<?php include('5.html'); ?>
 
 <?php
 include_once('_parts/footer.php');
