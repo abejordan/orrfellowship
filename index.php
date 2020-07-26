880acdd09348ac778e74fdba1e0bfe5caf8ae29d
Rhett Miller <rmiller@sharpencx.com>
2020-07-26 16:09:37 -0400

    added sections 6-8, removed container margins, misc fixes

--- index.php
+++ index.php
@@ -7,10 +7,27 @@ include_once('_parts/header.php');
 
 <!-- rhett is gettin' fancy -->
 <?php include('1.html'); ?>
+<br>
+<br>
 <?php include('2.html'); ?>
+<br>
+<br>
 <?php include('3.html'); ?>
+<br>
+<br>
 <?php include('4.html'); ?>
+<br>
+<br>
 <?php include('5.html'); ?>
+<br>
+<br>
+<?php include('6.html'); ?>
+<br>
+<br>
+<?php include('7.html'); ?>
+<br>
+<br>
+<?php include('8.html'); ?>
 
 <?php
 include_once('_parts/footer.php');
